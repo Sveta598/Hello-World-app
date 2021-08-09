d198725f3119f06032435d4d2548c86913f95a85
Svetlana <s.pavlovskaya@itechart-group.com>
2021-08-09 15:03:55 +0300

    vector icons

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SavedInput from './SavedInput';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Icon name="rocket" size={30} color="#900" />
       <SavedInput/>
       <Button
         title="Go to Details"
