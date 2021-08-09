e2873fa305158226cf84c558e9fbbe852f1f2507
Svetlana <s.pavlovskaya@itechart-group.com>
2021-08-09 14:03:00 +0300

    async storage

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
- import * as React from 'react';
- import { Button, View, Text } from 'react-native';
- import { NavigationContainer } from '@react-navigation/native';
- import { createNativeStackNavigator } from '@react-navigation/native-stack';
- 
- function HomeScreen({ navigation }) {
-   return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Text>Home Screen</Text>
-       <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-       />
-    </View>
-   );
- }
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SavedInput from './SavedInput';
 
- function DetailsScreen({ navigation }) {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <SavedInput/>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
@@ -32,8 +34,8 @@
     </View>
   );
 }
- 
- const Stack = createNativeStackNavigator();
+
+const Stack = createNativeStackNavigator();
  
  function App() {
   return (
@@ -66,5 +68,5 @@
     </NavigationContainer>
   );
 }
- 
- export default App;
+
+export default App;
