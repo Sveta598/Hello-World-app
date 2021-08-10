a95b2a1e9f446ee3f726f1f2f6b4edbf63ab600c
Svetlana <s.pavlovskaya@itechart-group.com>
2021-08-10 12:07:16 +0300

    Task 6

--- SavedInput.js
+++ SavedInput.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Input from './Input'
 
 const STORAGE_KEY = 'STRING_DATA'
