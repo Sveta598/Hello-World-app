bd17fdc8ed2335debb3af06d1afa239aae3fb696
Svetlana <s.pavlovskaya@itechart-group.com>
2021-08-11 11:25:33 +0300

    fonts

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SavedInput from './SavedInput';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider, Button } from 'react-native-elements';
+import Theme from './App/Theme/index'
 
 function HomeScreen({ navigation }) {
   return (
@@ -11,10 +14,12 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
       <Icon name="rocket" size={30} color="#900" />
       <SavedInput/>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <ThemeProvider theme={Theme}>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </ThemeProvider>
     </View>
   );
 }
@@ -23,16 +28,19 @@ function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
+      <ThemeProvider theme={Theme}>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push('Details')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Image source={require('./App/Assets/Images/home.png')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+        <Button
+          title="Go back to first screen in stack"
+          onPress={() => navigation.popToTop()}
+        />
+      </ThemeProvider>
     </View>
   );
 }
@@ -41,33 +49,35 @@ const Stack = createNativeStackNavigator();
  
  function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} 
-          options={{ 
-            title: 'My home', 
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} 
-          options={{ 
-            headerStyle: {
-              backgroundColor: '#51bbd9',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} 
+            options={{ 
+              title: 'My home', 
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              }
+            }}
+          />
+          <Stack.Screen name="Details" component={DetailsScreen} 
+            options={{ 
+              headerStyle: {
+                backgroundColor: '#51bbd9',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              }
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
---
+++ App/Theme/index.js
@@ -0,0 +1,13 @@
+const Theme = {
+    Button: {
+      titleStyle: {
+        color: '#630c51',
+        fontFamily: 'SFProRounded-Bold'
+      },
+      buttonStyle: {
+        backgroundColor: '#f2b7e6',
+      },
+    },
+  };
+
+  export default Theme
