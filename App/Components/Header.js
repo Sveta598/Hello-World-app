96f4f96d1eebbc1bd9feb87f2fe9287f5ef9071f
Dima Tretyak <itretyak@ya.ru>
2021-08-17 21:49:17 +0300

    fix: cover header into View

--- App/Components/Header.js
+++ App/Components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Header as RNHeader} from '@react-navigation/elements';
+import {View} from 'react-native';
 
 function Header(props) {
   return (
-    <RNHeader
-      headerTitleAlign="center"
-      headerStyle={{backgroundColor: '#d73374'}}
-      headerLeftContainerStyle={{paddingLeft: 15}}
-      headerRightContainerStyle={{paddingRight: 15}}
-      {...props}
-    />
+    <View>
+      <RNHeader
+        headerTitleAlign="center"
+        headerStyle={{backgroundColor: '#d73374'}}
+        headerLeftContainerStyle={{paddingLeft: 15}}
+        headerRightContainerStyle={{paddingRight: 15}}
+        {...props}
+      />
+    </View>
   );
 }
 
